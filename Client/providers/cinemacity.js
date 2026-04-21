f498e04292f2f2a99002a2853836208c5728fe0c
rabygbox <rabyg.box@gmail.com>
2026-04-21 12:41:27 +0200

    Update cinemacity.js

--- Client/providers/cinemacity.js
+++ Client/providers/cinemacity.js
@@ -178,7 +178,7 @@ function getStreams(tmdbId, mediaType, season, episode) {
         if (!url || !url.startsWith("http") || url.length < 15)
           return;
         streams.push({
-          name: "🌐",
+          name: "🍿 Multi",
           title: `${title} 🌐`,
           url,
           quality: quality || extractQuality(url),
