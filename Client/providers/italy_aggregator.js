c5eea72606260e99bf22e8595554b4c5bfba56fc
rabygbox <rabyg.box@gmail.com>
2026-04-21 11:09:57 +0200

    Create italy_aggregator.js

---
+++ Client/providers/italy_aggregator.js
@@ -0,0 +1,19 @@
+{
+  "name": "Italy Combined",
+  "version": "1.1.57",
+  "scrapers": [
+    {
+      "id": "easystreams-italy-all",
+      "name": "Italy All-in-One",
+      "version": "1.1.57",
+      "description": "Durchsucht alle italienischen Quellen gleichzeitig",
+      "resources": ["stream"],
+      "contentLanguage": ["it"],
+      "formats": ["mp4", "m3u8"],
+      "idPrefixes": ["tmdb:", "tt", "kitsu:"],
+      "filename": "providers/italy_aggregator.js",
+      "supportedTypes": ["movie", "tv", "anime"],
+      "enabled": true
+    }
+  ]
+}
