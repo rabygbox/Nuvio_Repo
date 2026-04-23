3d106134dbacbac616a64d50a99b9c123f22011b
rabygbox <rabyg.box@gmail.com>
2026-04-23 23:25:44 +0200

    Update guardahd.js

--- Client/providers/guardahd.js
+++ Client/providers/guardahd.js
@@ -7425,7 +7425,7 @@ var require_formatter = __commonJS({
         pName = pName.charAt(0).toUpperCase() + pName.slice(1);
       }
       if (pName) {
-        pName = `\u{1F4E1} ${pName}`;
+        pName = `${pName}`;
       }
       const behaviorHints = stream.behaviorHints && typeof stream.behaviorHints === "object" ? __spreadValues({}, stream.behaviorHints) : {};
       let finalHeaders = stream.headers;
