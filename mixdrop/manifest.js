8690d99ef70736988ef5b29e38838f89e464a5ee
rabygbox <rabyg.box@gmail.com>
2026-04-28 20:30:55 +0200

    Update manifest.js

--- mixdrop/manifest.js
+++ mixdrop/manifest.js
@@ -1,23 +1,8 @@
 {
-  "id": "it.lisa.mixdrop.personal",
+  "id": "it.lisa.mixdrop",
   "version": "1.0.0",
-  "name": "I miei film su Mixdrop",
-  "description": "Visualizza i file dal mio account Mixdrop",
-  "resources": [
-    "catalog",
-    "stream"
-  ],
-  "types": [
-    "movie",
-    "series",
-    "other"
-  ],
-  "catalogs": [
-    {
-      "type": "movie",
-      "id": "mixdrop_catalog",
-      "name": "Mixdrop Library"
-    }
-  ],
-  "idPrefixes": ["tt"]
+  "name": "Mixdrop Personal",
+  "resources": ["stream"],
+  "types": ["movie"],
+  "idPrefixes": ["tt"] 
 }
