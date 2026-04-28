59c47c89f2117b0417e410346a649f733380f54c
rabygbox <rabyg.box@gmail.com>
2026-04-28 19:54:20 +0200

    Add manifest.js for Mixdrop extension
    Initial manifest file for Mixdrop Personal Library.

---
+++ mixdrop/manifest.js
@@ -0,0 +1,9 @@
+{
+  "id": "org.tuonome.mixdrop",
+  "version": "1.0.0",
+  "name": "Mixdrop Personal Library",
+  "description": "Cerca e riproduce i tuoi file personali caricati su Mixdrop",
+  "resources": ["catalog", "stream"],
+  "types": ["movie", "series"],
+  "idPrefixes": ["md_"]
+}
