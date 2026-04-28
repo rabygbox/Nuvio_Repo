a969fc204674970b833edb6cdfdcab0c3cf7a0d4
rabygbox <rabyg.box@gmail.com>
2026-04-28 20:38:45 +0200

    Update version and resources in manifest.js

--- mixdrop/manifest.js
+++ mixdrop/manifest.js
@@ -1,8 +1,9 @@
 {
   "id": "it.lisa.mixdrop",
-  "version": "1.0.0",
+  "version": "1.0.1",
   "name": "Mixdrop Personal",
-  "resources": ["stream"],
+  "description": "I miei film caricati su Mixdrop",
+  "resources": ["stream", "catalog"],
   "types": ["movie"],
-  "idPrefixes": ["tt"] 
+  "idPrefixes": ["tt"]
 }
